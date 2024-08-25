f2bbf28c7714a832d19000f8e20853b99e4e2431
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2024-08-25 20:24:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def upload_image():
         return jsonify({'error': 'An error occurred: ' + str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
