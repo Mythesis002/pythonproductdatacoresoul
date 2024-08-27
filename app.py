0277a2bba8b54c794ecadb5192257faf4a1f8ac9
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2024-08-27 20:09:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from gradio_client import Client, handle_file
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)  # Enable CORS for all routes
 
 # Set up Cloudinary credentials
 cloudinary.config(
