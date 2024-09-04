02db477b8414dd62220bd0640c7170d8f8a17479
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2024-09-04 21:39:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ cloudinary.config(
 )
 
 # Initialize Gradio client
-client = Client("ChrisJohnson111/test333")
+client = Client("yisol/IDM-VTON")
 
 @app.route('/uploaded', methods=['POST'])
 def upload_image():
